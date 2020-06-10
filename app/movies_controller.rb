bf1a093544b2143353cfcbf7028a147d688c016a
Irene Scott <irenejinscott@gmail.com>
2020-06-10 22:51:40 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,7 +33,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = attributes[:title]
     m.release_date = attributes[:release_date]
-    save
+    m.save
   end
 end
 
